714e8a46f80999d8a67e5df1b3c9ec240c55566f
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-11-25 12:10:05 +0300

    Aritmetiksel Operatörler

--- app/src/main/java/com/example/temelkomutlarunite_3/MainActivity.java
+++ app/src/main/java/com/example/temelkomutlarunite_3/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, Uyg7Activity.class);
-
+                startActivity(i);
             }
         } );
     }
